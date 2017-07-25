ce72296d4b4c229ed0a7d9c66e4d3f0181745636
federicosilva <federicosilva@mbp-de-federico.guest.fing.edu.uy>
2017-07-25 16:49:08 -0300

    Testbench finished

--- MedidorPotencia/main.c
+++ MedidorPotencia/main.c
@@ -1,50 +1,23 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
+/*
+ * Medidor de Potencia
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Testeo de interrupciones desde GPIO
  *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-/******************************************************************************
- * MSP432 Empty Project
- *
- * Description: An empty project that uses DriverLib
+ * Se generan interrupciones en cada flanco de GPIO. Dentro de la interrupción
+ * se niega la salida Output, produciendo entonces una señal de la mitad de
+ * frecuencia.
  *
  *                MSP432P401
  *             ------------------
  *         /|\|                  |
  *          | |                  |
- *          --|RST               |
+ *          --|RST          P1.1 | <-- GPIO req
+ *            |             P1.2 | --> Output
  *            |                  |
  *            |                  |
  *            |                  |
  *            |                  |
- *            |                  |
- * Author: 
+ * Author: EAEU
 *******************************************************************************/
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
@@ -58,8 +31,48 @@ int main(void)
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
 
+    // Seteo 1.2 como salida
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN2);
+
+    // Configuro 1.1 como entrada con resistencia de pull-up
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+
+    // Limpio bandera de interrupciones y activo las del puerto 1.1
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+
+    // Activo las interrupciones generales del puerto 1
+    MAP_Interrupt_enableInterrupt(INT_PORT1);
+
+    // Habilito interrupciones
+    MAP_Interrupt_enableMaster();
+
+
     while(1)
     {
         
     }
+}
+
+/*
+ * ISR del puerto 1
+ * (Notar que es del puerto 1 en su totalidad, de ahí que debe verificar
+ * que efectivamente provenga del P1.1).
+ */
+void PORT1_IRQHandler(void)
+{
+    uint32_t status;
+
+    // Obtengo todo el vector estado de las interrupciones del puerto 1
+    status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
+    // Limipio la que me llama
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, status);
+
+    // Verifico que provenga del pin 1 efectivamente (trivial en este caso)
+    if(status & GPIO_PIN1)
+    {
+        // Niego el estado del pin de salida
+        MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN2);
+    }
+
 }
