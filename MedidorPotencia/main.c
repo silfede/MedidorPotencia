50f05c04f8788c6a4cb473fcf572553bff9c9085
federicosilva <federicosilva@mbp-de-federico>
2017-08-13 17:16:28 -0300

    Ping Pong working on new Launchpad
    Still having problems to set de frequency correctly.

--- MedidorPotencia/main.c
+++ MedidorPotencia/main.c
@@ -1,7 +1,7 @@
 /*
  * Medidor de Potencia
  *
- * Testeo de interrupciones desde GPIO y velocidad del ADC
+ * Testeo de interrupciones desde GPIO y velocidad del ADC. Se suma ahora el uso del DMA.
  *
  * Se generan interrupciones en cada flanco de GPIO. Dentro de la interrupción
  * se activa el ADC en modo continuo, dos canales. En la interrupción se lleva
@@ -26,28 +26,46 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define SAMPLE_LENGTH 512
+
+/* DMA Control Table */
+#ifdef ewarm
+#pragma data_alignment=256
+#else
+#pragma DATA_ALIGN(controlTable, 256)
+#endif
+uint8_t controlTable[256];
+
 static uint32_t N=0;
 
 volatile uint16_t N_ADC[128];
 static uint8_t index=0;
 
+uint16_t voltaje[SAMPLE_LENGTH];
+uint16_t corriente[SAMPLE_LENGTH];
+static volatile uint32_t mclk;
 int main(void)
 {
     /* Stop Watchdog  */
     MAP_WDT_A_holdTimer();
 
+    memset(voltaje, 0x00, SAMPLE_LENGTH);
+    memset(corriente, 0x00, SAMPLE_LENGTH);
+    memset(controlTable, 0x00, 1024);
+
     /****************************************************************************
      ************************CONFIGURACIÓN DE RELOJES****************************
      ****************************************************************************/
-
+    MAP_FPU_enableModule();
     // Seteo el DCO a máxima frecuencia 48 MHz
 
-    MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
+    //MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
+    //MAP_CS_setDCOFrequency(48000000);
 
     // Asigno el DCO como Master Clock
     MAP_CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
     /****************************************************************************/
-
+    mclk = CS_getMCLK();
 
     /****************************************************************************
      ************************CONFIGURACIÓN DE GPIO*******************************
@@ -64,7 +82,7 @@ int main(void)
     MAP_GPIO_interruptEdgeSelect(GPIO_PORT_P1,GPIO_PIN5,GPIO_LOW_TO_HIGH_TRANSITION);
 
     // Activo las interrupciones generales del puerto 1
-    MAP_Interrupt_enableInterrupt(INT_PORT1);
+    //MAP_Interrupt_enableInterrupt(INT_PORT1);
     /****************************************************************************/
 
     /****************************************************************************
@@ -84,25 +102,56 @@ int main(void)
     // Configuro conversión secuencial de memoria 0 y 1, en bucle
     MAP_ADC14_configureMultiSequenceMode(ADC_MEM0, ADC_MEM1, true);
 
+
     // Asigno las memorias a los canales del ADC, referencia AVCC
-    MAP_ADC14_configureConversionMemory(ADC_MEM0,
-            ADC_VREFPOS_AVCC_VREFNEG_VSS,
+    MAP_ADC14_configureConversionMemory(ADC_MEM0,ADC_VREFPOS_AVCC_VREFNEG_VSS,
+            ADC_INPUT_A0, false);
+    MAP_ADC14_configureConversionMemory(ADC_MEM1,ADC_VREFPOS_AVCC_VREFNEG_VSS,
             ADC_INPUT_A1, false);
-    MAP_ADC14_configureConversionMemory(ADC_MEM1,
-            ADC_VREFPOS_AVCC_VREFNEG_VSS,
-            ADC_INPUT_A2, false);
 
     // Cambia entre canales automáticamente
     MAP_ADC14_enableSampleTimer(ADC_AUTOMATIC_ITERATION);
 
-    // Habilito interrupciones del puerto (son con respecto a la memoria)
-    MAP_ADC14_enableInterrupt(ADC_INT1);
+    /****************************************************************************/
 
-    // Habilito interrupciones del ADC
-    MAP_Interrupt_enableInterrupt(INT_ADC14);
+    /****************************************************************************
+     **************************CONFIGURACIÓN DE DMA******************************
+     ****************************************************************************/
+    MAP_DMA_enableModule();
+    MAP_DMA_setControlBase(controlTable);
+
+    MAP_DMA_assignChannel(DMA_CH7_ADC14);
+
+    MAP_DMA_disableChannelAttribute(DMA_CH7_ADC14,
+                                  UDMA_ATTR_ALTSELECT | UDMA_ATTR_USEBURST |
+                                  UDMA_ATTR_HIGH_PRIORITY |
+                                  UDMA_ATTR_REQMASK);
+    MAP_DMA_enableChannelAttribute(DMA_CH7_ADC14, UDMA_ATTR_USEBURST);
+
+
+    MAP_DMA_setChannelControl(UDMA_PRI_SELECT | DMA_CH7_ADC14,
+        UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
+    MAP_DMA_setChannelTransfer(UDMA_PRI_SELECT | DMA_CH7_ADC14,
+        UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[0],
+        voltaje, SAMPLE_LENGTH);
+
+    MAP_DMA_setChannelControl(UDMA_ALT_SELECT | DMA_CH7_ADC14,
+        UDMA_SIZE_16 | UDMA_SRC_INC_NONE | UDMA_DST_INC_16 | UDMA_ARB_1);
+    MAP_DMA_setChannelTransfer(UDMA_ALT_SELECT | DMA_CH7_ADC14,
+        UDMA_MODE_PINGPONG, (void*) &ADC14->MEM[1],
+        corriente, SAMPLE_LENGTH);
+
     
+
+    MAP_DMA_assignInterrupt(DMA_INT1, 7);
+    MAP_Interrupt_enableInterrupt(INT_DMA_INT1);
+
+    MAP_DMA_enableChannel(7);
+
+
     // Habilito interrupciones generales
     MAP_Interrupt_enableMaster();
+
 
     // Inicio conversión
     ADC14->CTL0 |= ADC14_CTL0_ENC | ADC14_CTL0_SC;
@@ -131,7 +180,7 @@ void PORT1_IRQHandler(void)
 }
 
 
-void ADC14_IRQHandler(void)
+/*void ADC14_IRQHandler(void)
 {
     if(ADC14->IFGR0 & ADC14_IFGR0_IFG1)
     {
@@ -140,4 +189,10 @@ void ADC14_IRQHandler(void)
         ADC14->CLRIFGR0 |= ADC14_IFGR0_IFG1 | ADC14_IFGR0_IFG0;
     }
 
+}*/
+
+void DMA_INT1_IRQHandler(void)
+{
+    N=N+1;
+    MAP_DMA_clearInterruptFlag(7);
 }
